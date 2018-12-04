d7fd4316a9d9a762383fc5e1f19e0f7c14afbc7c
YoonjuSeo <yoonjuseo08@gmail.com>
2018-12-03 22:49:58 -0800

    lesson42 181203

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker'
-import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import rootReducer from './reducers/rootReducer'
 
 const store = createStore(rootReducer);
