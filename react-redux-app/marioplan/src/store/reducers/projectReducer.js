f750e4cfccd417580cb6b3e2ab12c94fe4ead3d3
YoonjuSeo <yoonjuseo08@gmail.com>
2019-01-26 16:06:39 +0900

    marioplan12 190126

--- react-redux-app/marioplan/src/store/reducers/projectReducer.js
+++ react-redux-app/marioplan/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects:[
+        {id: '1', title: 'help me find peach', content: 'You can find her in level 10'},
+        {id: '2', title: 'collect all the stars', content: 'You can get a bonus points!'},
+        {id: '3', title: 'egg hunt with yoshi', content: 'You can ride him on the back. Have fun!'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
