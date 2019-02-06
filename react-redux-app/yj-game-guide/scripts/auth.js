474aba76ff7c22657dc2117d626ec782028083dd
YoonjuSeo <yoonjuseo08@gmail.com>
2019-02-06 13:59:27 +0900

    gameguide12 190206;

--- react-redux-app/yj-game-guide/scripts/auth.js
+++ react-redux-app/yj-game-guide/scripts/auth.js
@@ -12,6 +12,26 @@ auth.onAuthStateChanged(user => {
     }
   });
 
+
+//create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content']
+    }).then(() => {
+        //close the modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    }).catch(err => {
+        console.log(err.message)
+    })
+})
+
+
 //signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
