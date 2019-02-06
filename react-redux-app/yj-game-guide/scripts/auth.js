5887951fcb9d041789bbc7007c3785cc2d1cb159
YoonjuSeo <yoonjuseo08@gmail.com>
2019-02-06 14:26:00 +0900

    gameguide14 190206

--- react-redux-app/yj-game-guide/scripts/auth.js
+++ react-redux-app/yj-game-guide/scripts/auth.js
@@ -1,5 +1,6 @@
 //listen for auth status changes
 auth.onAuthStateChanged(user => {
+    console.log(user);
     if (user) {
         //get data from firebase.google.com
         db.collection('guides').onSnapshot(snapshot => {
